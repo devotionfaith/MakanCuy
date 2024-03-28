f9f0ef9c8b37b3fada9ca3d195276bdbcfc8a386
devotionfaith <rgusti530@gmail.com>
2024-03-28 12:46:48 +0700

    Adding database and shared preference

---
+++ app/src/main/java/com/devotion/makancuy/data/source/local/pref/UserPreference.kt
@@ -0,0 +1,25 @@
+package com.devotion.makancuy.data.source.local.pref
+
+import android.content.Context
+import com.devotion.makancuy.utils.SharedPreferenceUtils
+import com.devotion.makancuy.utils.SharedPreferenceUtils.set
+
+interface UserPreference {
+    fun isUsingGridMode() : Boolean
+    fun setUsingGridMode(isUsingGridMode: Boolean)
+}
+
+class UserPreferenceImpl(private val context: Context) : UserPreference{
+    private val pref = SharedPreferenceUtils.createPreference(context, PREF_NAME)
+    override fun isUsingGridMode(): Boolean = pref.getBoolean(KEY_IS_USING_GRID_MODE, false)
+
+    override fun setUsingGridMode(isUsingGridMode: Boolean) {
+        pref[KEY_IS_USING_GRID_MODE] = isUsingGridMode
+    }
+
+    companion object{
+        const val PREF_NAME = "makancuy-pref"
+        const val KEY_IS_USING_GRID_MODE = "KEY_IS_USING_GRID_MODE"
+    }
+
+}
