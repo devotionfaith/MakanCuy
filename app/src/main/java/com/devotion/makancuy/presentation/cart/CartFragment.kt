4597088d4ab187ee6f3d302b3710c6fe92d52fcf
devotionfaith <rgusti530@gmail.com>
2024-03-29 19:31:53 +0700

    Adding Check Out Activity

--- app/src/main/java/com/devotion/makancuy/presentation/cart/CartFragment.kt
+++ app/src/main/java/com/devotion/makancuy/presentation/cart/CartFragment.kt
@@ -1,16 +1,59 @@
 package com.devotion.makancuy.presentation.cart
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import com.devotion.makancuy.R
+import com.devotion.makancuy.data.datasource.cart.CartDataSource
+import com.devotion.makancuy.data.datasource.cart.CartDatabaseDataSource
+import com.devotion.makancuy.data.model.Cart
+import com.devotion.makancuy.data.repository.CartRepository
+import com.devotion.makancuy.data.repository.CartRepositoryImpl
+import com.devotion.makancuy.data.source.local.database.AppDatabase
 import com.devotion.makancuy.databinding.FragmentCartBinding
+import com.devotion.makancuy.presentation.checkout.CheckoutActivity
+import com.devotion.makancuy.presentation.common.adapter.CartListAdapter
+import com.devotion.makancuy.presentation.common.adapter.CartListener
+import com.devotion.makancuy.utils.GenericViewModelFactory
+import com.devotion.makancuy.utils.hideKeyboard
+import com.devotion.makancuy.utils.proceedWhen
+import com.devotion.makancuy.utils.toIndonesianFormat
 
 class CartFragment : Fragment() {
-
     private lateinit var binding: FragmentCartBinding
+
+    private val viewModel: CartViewModel by viewModels {
+        val db = AppDatabase.getInstance(requireContext())
+        val ds: CartDataSource = CartDatabaseDataSource(db.cartDao())
+        val rp: CartRepository = CartRepositoryImpl(ds)
+        GenericViewModelFactory.create(CartViewModel(rp))
+    }
+
+    private val adapter: CartListAdapter by lazy {
+        CartListAdapter(object : CartListener {
+            override fun onPlusTotalItemCartClicked(cart: Cart) {
+                viewModel.increaseCart(cart)
+            }
+
+            override fun onMinusTotalItemCartClicked(cart: Cart) {
+                viewModel.decreaseCart(cart)
+            }
+
+            override fun onRemoveCartClicked(cart: Cart) {
+                viewModel.removeCart(cart)
+            }
+
+            override fun onUserDoneEditingNotes(cart: Cart) {
+                viewModel.setCartNotes(cart)
+                hideKeyboard()
+            }
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +61,63 @@ class CartFragment : Fragment() {
     ): View {
         binding = FragmentCartBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupList()
+        observeData()
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.btnCheckout.setOnClickListener {
+            startActivity(Intent(requireContext(), CheckoutActivity::class.java))
+        }
+    }
+
+    private fun observeData() {
+        viewModel.getCartList().observe(viewLifecycleOwner) { result ->
+            result.proceedWhen(
+                doOnLoading = {
+                    binding.layoutState.root.isVisible = true
+                    binding.layoutState.pbLoading.isVisible = true
+                    binding.layoutState.tvError.isVisible = false
+                    binding.rvCart.isVisible = false
+                },
+                doOnSuccess = {
+                    binding.layoutState.root.isVisible = false
+                    binding.layoutState.pbLoading.isVisible = false
+                    binding.layoutState.tvError.isVisible = false
+                    binding.rvCart.isVisible = true
+                    result.payload?.let { (carts, totalPrice) ->
+                        adapter.submitData(carts)
+                        binding.tvTotalPrice.text = totalPrice.toIndonesianFormat()
+                    }
+                },
+                doOnError = {
+                    binding.layoutState.root.isVisible = true
+                    binding.layoutState.pbLoading.isVisible = false
+                    binding.layoutState.tvError.isVisible = true
+                    binding.layoutState.tvError.text = result.exception?.message.orEmpty()
+                    binding.rvCart.isVisible = false
+                },
+                doOnEmpty = {
+                    binding.layoutState.root.isVisible = true
+                    binding.layoutState.pbLoading.isVisible = false
+                    binding.layoutState.tvError.isVisible = true
+                    binding.layoutState.tvError.text = getString(R.string.text_cart_is_empty)
+                    binding.rvCart.isVisible = false
+                    result.payload?.let { (carts, totalPrice) ->
+                        binding.tvTotalPrice.text = totalPrice.toIndonesianFormat()
+                    }
+                }
+            )
+        }
+    }
+
+    private fun setupList() {
+        binding.rvCart.adapter = this@CartFragment.adapter
     }
 
 }
---
+++ app/src/main/java/com/devotion/makancuy/presentation/checkout/adapter/PriceListAdapter.kt
@@ -0,0 +1,63 @@
+package com.devotion.makancuy.presentation.checkout.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.devotion.makancuy.data.model.PriceItem
+import com.devotion.makancuy.databinding.ItemOrderBinding
+import com.devotion.makancuy.utils.toIndonesianFormat
+
+class PriceListAdapter(private val itemClick: (PriceItem) -> Unit) :
+    RecyclerView.Adapter<PriceListAdapter.PriceItemViewHolder>() {
+
+    private val dataDiffer =
+        AsyncListDiffer(
+            this,
+            object : DiffUtil.ItemCallback<PriceItem>() {
+                override fun areItemsTheSame(
+                    oldItem: PriceItem,
+                    newItem: PriceItem
+                ): Boolean {
+                    return oldItem.name == newItem.name
+                }
+
+                override fun areContentsTheSame(
+                    oldItem: PriceItem,
+                    newItem: PriceItem
+                ): Boolean {
+                    return oldItem.hashCode() == newItem.hashCode()
+                }
+            }
+        )
+
+    fun submitData(data: List<PriceItem>) {
+        dataDiffer.submitList(data)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PriceItemViewHolder {
+        val binding = ItemOrderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PriceItemViewHolder(binding, itemClick)
+    }
+
+    override fun onBindViewHolder(holder: PriceItemViewHolder, position: Int) {
+        holder.bindView(dataDiffer.currentList[position])
+    }
+
+    override fun getItemCount(): Int = dataDiffer.currentList.size
+
+    class PriceItemViewHolder(
+        private val binding: ItemOrderBinding,
+        val itemClick: (PriceItem) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bindView(item: PriceItem) {
+            with(item) {
+                binding.tvOrderPrice.text = item.total.toIndonesianFormat()
+                binding.tvOrderName.text = item.name
+                itemView.setOnClickListener { itemClick(this) }
+            }
+        }
+    }
+}
