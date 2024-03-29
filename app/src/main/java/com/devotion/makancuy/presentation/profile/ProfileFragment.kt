2c827dc17821c25b28656310e280257e7f44cda2
devotionfaith <rgusti530@gmail.com>
2024-03-29 19:53:47 +0700

    Adding Profile

--- app/src/main/java/com/devotion/makancuy/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/devotion/makancuy/presentation/profile/ProfileFragment.kt
@@ -5,18 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.devotion.makancuy.R
 import com.devotion.makancuy.databinding.FragmentCartBinding
 import com.devotion.makancuy.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
     private lateinit var binding: FragmentProfileBinding
+
+    private val viewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+        observeEditMode()
+        observeProfileData()
+    }
+
+    private fun observeProfileData() {
+        viewModel.profileData.observe(viewLifecycleOwner) {
+            binding.ivProfile.load(it.profileImg) {
+                crossfade(true)
+                error(R.drawable.ic_tab_profile)
+                transformations(CircleCropTransformation())
+            }
+            binding.nameEditText.setText(it.name)
+            binding.usernameEditText.setText(it.username)
+            binding.emailEditText.setText(it.email)
+        }
+    }
+
+    private fun setClickListener() {
+        binding.btnEdit.setOnClickListener {
+            viewModel.changeEditMode()
+        }
+    }
+
+    private fun observeEditMode() {
+        viewModel.isEditMode.observe(viewLifecycleOwner) {
+            binding.emailEditText.isEnabled = it
+            binding.nameEditText.isEnabled = it
+            binding.usernameEditText.isEnabled = it
+            binding.btnEdit.text = if (it) "Save" else "Edit"
+        }
     }
 }
--- app/src/main/java/com/devotion/makancuy/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/devotion/makancuy/presentation/profile/ProfileViewModel.kt
@@ -1,6 +1,24 @@
 package com.devotion.makancuy.presentation.profile
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.devotion.makancuy.data.model.Profile
 
 class ProfileViewModel : ViewModel() {
+
+    val profileData = MutableLiveData(
+        Profile(
+            name = "Rizky Gustiantoro",
+            username = "rgustiann",
+            email = "rgusti530@gmail.com",
+            profileImg = "https://instagram.fjog1-1.fna.fbcdn.net/v/t51.2885-19/364080408_829727922079943_5389332852020861258_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fjog1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=bnvlIBQYzyAAX8dLosF&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfBoouIT5iNjJRQoVvC92otlS1kTXbk08kr0U6W2528WeQ&oe=660B8BC4&_nc_sid=8b3546"
+        )
+    )
+
+    val isEditMode = MutableLiveData(false)
+
+    fun changeEditMode() {
+        val currentValue = isEditMode.value ?: false
+        isEditMode.postValue(!currentValue)
+    }
 }
