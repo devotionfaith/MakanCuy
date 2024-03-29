c4f97496993f00a53cf03b0d4928d254bc974713
devotionfaith <rgusti530@gmail.com>
2024-03-29 22:52:16 +0700

    Adding Shared Preference

--- app/src/main/java/com/devotion/makancuy/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/devotion/makancuy/presentation/home/HomeViewModel.kt
@@ -5,18 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.devotion.makancuy.data.repository.CategoryRepository
 import com.devotion.makancuy.data.repository.MenuRepository
+import com.devotion.makancuy.data.source.local.pref.UserPreference
 
 class HomeViewModel(
     private val categoryRepository: CategoryRepository,
-    private val menuRepository: MenuRepository
+    private val menuRepository: MenuRepository,
+    private val userPreference: UserPreference
 ) : ViewModel() {
-    private val _isUsingGridMode = MutableLiveData(false)
+    private val _isUsingGridMode = MutableLiveData(userPreference.isUsingGridMode())
     val isUsingGridMode: LiveData<Boolean>
         get() = _isUsingGridMode
 
     fun changeListMode() {
         val currentValue = _isUsingGridMode.value ?: false
         _isUsingGridMode.postValue(!currentValue)
+        userPreference.setUsingGridMode(!currentValue)
     }
 
     fun getCategory() = categoryRepository.getCategories()
