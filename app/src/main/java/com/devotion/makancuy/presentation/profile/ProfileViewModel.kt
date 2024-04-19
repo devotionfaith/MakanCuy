ff9707ffc705ff56785833b88bf76c937f07f396
devotionfaith <rgusti530@gmail.com>
2024-04-19 22:04:24 +0700

    Adding Change Username and Email

--- app/src/main/java/com/devotion/makancuy/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/devotion/makancuy/presentation/profile/ProfileViewModel.kt
@@ -7,32 +7,16 @@ import com.devotion.makancuy.data.model.Profile
 import com.devotion.makancuy.data.repository.UserRepository
 import kotlinx.coroutines.Dispatchers
 
-class ProfileViewModel(private val userRepository: UserRepository)  : ViewModel() {
+class ProfileViewModel(private val userRepository: UserRepository) : ViewModel() {
 
-    val profileData = MutableLiveData(
-        Profile(
-            name = "Rizky Gustiantoro",
-            username = "rgustiann",
-            email = "rgusti530@gmail.com",
-            profileImg = "https://instagram.fjog1-1.fna.fbcdn.net/v/t51.2885-19/364080408_829727922079943_5389332852020861258_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fjog1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=bnvlIBQYzyAAX8dLosF&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfBoouIT5iNjJRQoVvC92otlS1kTXbk08kr0U6W2528WeQ&oe=660B8BC4&_nc_sid=8b3546"
-        )
-    )
+    fun getCurrentUser() = userRepository.getCurrentUser()
+    fun updateUsername(newName : String) = userRepository.updateProfile(
+        fullName = newName
+    ).asLiveData(Dispatchers.IO)
 
+    fun updateEmail(newEmail : String) = userRepository.updateEmail(
+        newEmail = newEmail
+    ).asLiveData(Dispatchers.IO)
     fun doLogout() = userRepository.doLogout().asLiveData(Dispatchers.IO)
-    val isEditMode = MutableLiveData(false)
-    val buttonText = MutableLiveData("Edit")
 
-    fun changeEditMode() {
-        val currentValue = isEditMode.value ?: false
-        isEditMode.postValue(!currentValue)
-    }
-
-    fun getButtonText() {
-        val currentValue = isEditMode.value ?: false
-        return if (!currentValue) {
-            buttonText.postValue("Edit")
-        } else {
-            buttonText.postValue("Edit")
-        }
-    }
 }
